87866ea79be66ea4a104b971961a1775a8acb00d
Michael Pszonka <pszonkadonk@Michaels-Air.home>
2017-04-22 21:07:59 -0400

    add plotting and addition data scraping

---
+++ regression.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+
+def currency_fmt(x, y):
+    x = int(x)
+    return "${0:,}".format(x)
+
+def run_regression_price_squarefeet(filename):
+    
+    home_data = pd.read_csv(filename, encoding="latin1")
+    home_data = home_data[home_data.price < 1500000]
+    home_data = home_data[home_data.square_feet < 2500]
+    home_data_matrix = home_data.as_matrix()
+
+    price = home_data[['price']]
+    square_feet = home_data[['square_feet']]
+    
+    model = LinearRegression().fit(price, square_feet)
+
+
+    m = model.coef_[0]
+    b = model.intercept_
+    print("formula y= {0}x + {1}".format(m, b))
+
+    ax = plt.subplot()
+    plt.xticks(fontsize= 8)
+    plt.yticks()
+    
+
+    plt.scatter(price, square_feet , color="purple", s=4.5)
+    ax.xaxis.set_major_formatter(tkr.FuncFormatter(currency_fmt))
+    
+
+    plt.plot(price, model.predict(price), color='black', linewidth=2)
+
+    plt.title("Jersey City Housing Price Against Square Feet")
+    plt.ylabel("Square Feet")
+    plt.xlabel("Price")
+
+    
+ 
+    
+    plt.show()
+
+def plot_price_years(filename):
+    home_data = pd.read_csv(filename, encoding="latin1")
+    home_data = home_data[home_data.price < 1500000]
+    home_data_matrix = home_data.as_matrix()
+
+    ax = plt.subplot()
+    ax.yaxis.set_major_formatter(tkr.FuncFormatter(currency_fmt))
+
+
+    homes_1870_to_1889 = home_data[(home_data.year >= 1870) & (home_data.year <=1889)]
+    homes_1890_to_1909 = home_data[(home_data.year >= 1890) & (home_data.year <=1909)]
+    homes_1910_to_1929 = home_data[(home_data.year >= 1910) & (home_data.year <=1929)]
+    homes_1930_to_1949 = home_data[(home_data.year >= 1930) & (home_data.year <=1949)]
+    homes_1930_to_1949 = home_data[(home_data.year >= 1930) & (home_data.year <=1949)]
+    homes_1950_to_1969 = home_data[(home_data.year >= 1950) & (home_data.year <=1969)]
+    homes_1970_to_1989 = home_data[(home_data.year >= 1970) & (home_data.year <=1989)]
+    homes_1990_to_2009 = home_data[(home_data.year >= 1990) & (home_data.year <=2009)]
+    homes_2010_to_2029 = home_data[(home_data.year >= 2010) & (home_data.year <=2029)]
+
+    plt.scatter(homes_1870_to_1889[['year']], homes_1870_to_1889[['price']], color = "red", label='1870-1889', s=25)
+    plt.scatter(homes_1890_to_1909[['year']], homes_1890_to_1909[['price']], color = "blue", label='1890-1909', s=25)
+    plt.scatter(homes_1910_to_1929[['year']], homes_1910_to_1929[['price']], color = "yellow", label='1910-1929', s=25)
+    plt.scatter(homes_1930_to_1949[['year']], [homes_1930_to_1949['price']], color = "green", label='1930-1949', s=25)
+    plt.scatter(homes_1950_to_1969[['year']], homes_1950_to_1969[['price']], color = "purple", label='1950-1969', s=25)
+    plt.scatter(homes_1970_to_1989[['year']], homes_1970_to_1989[['price']], color = "pink", label='1970-1989', s=25)
+    plt.scatter(homes_1990_to_2009[['year']], homes_1990_to_2009[['price']], color = "cyan", label='1990-2009', s=25)
+    plt.scatter(homes_2010_to_2029[['year']], homes_2010_to_2029[['price']], color = "magenta", label='2010-2029', s=25)
+
+    plt.legend(loc="upper right", fontsize=8, ncol=2)
+    
+    plt.xticks()
+    plt.yticks()
+
+    plt.title("Jersey City Homes | Year Built")
+    plt.ylabel("Price")
+    plt.xlabel("Year")
+
+    plt.show()
+
+
+
+
+
+if __name__=='__main__':
+
+    # home_data_string = "home_detail-cleaned.csv"
+    # run_regression_price_squarefeet(home_data_string)
+
+    home_data_string = "home_detail-cleaned-year.csv"
+    plot_price_years(home_data_string)
--- trulia_scrape_and_clean.py
+++ trulia_scrape_and_clean.py
@@ -1,6 +1,11 @@
 import requests, re
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
+import locale
+from locale import atof
+
+locale.setlocale(locale.LC_NUMERIC, '')
 
 def fetchhtml(url):
     PRETEND_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0'
@@ -14,21 +19,21 @@ def geturl(url,page):
 
 def write_to_csv(data): 
     print("writing data to file...")
-    with open('home_detail.csv', 'w', newline='') as csvfile:
+    with open('data-with-year-remaining.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data)   
 
 def scrapeData():
-    count = 1
+    count = 31
     home_inventory = []
 
     #For iterating through pages just set limitPage to number of pages you want to iterate
-    limitPage = 60
+    limitPage = 70  
 
     #Property count, will give you the total number of user properties scraped
     homeCount = 1
 
-    url = "https://www.trulia.com/for_sale/40.692466103506,40.79703022221,-74.294922954219,-74.075196391719_xy/12_zm/"
+    url = "https://www.trulia.com/NJ/Jersey_City/"
 
     while (count < limitPage):
         soup = fetchhtml(geturl(url,count))
@@ -46,15 +51,34 @@ def scrapeData():
             city = address.split(",")[0]
             state = address.replace(" ","").split(",")[1][0:2]
             zip_code = address.replace(" ","").split(",")[1][2:7]
-                        
-            details = user.find('ul', {'class': "listBulleted mbn"}).findAll('li')
+            square_feet = 'n/a'
+            year_built = 'n/a'
             
+            year_built_input = user.find('input', {'id': "property_detail_year_built_org"})
+
+
+            square_feet_input = user.find('input', {'id': "property_detail_sqft_org"})
+
+            if(square_feet_input != None):
+                square_feet = square_feet_input.get('value')
+
+            if(year_built_input != None):
+                year_built = year_built_input.get('value')
+            
+            
+            listedBulletin = user.find('ul', {'class': "listBulleted mbn"})
+
+            if(listedBulletin != None):
+                details = listedBulletin.findAll('li')
+            else:
+                details = []
+
             for li in details:
                 attributes.append(str(li).replace("<li>","")
                                         .replace("</li>","")
                                         .replace("Price: $",""))
 
-            home_detail = [link, street, city, state, zip_code]
+            home_detail = [link, street, city, state, zip_code, square_feet, year_built]
             home_detail.extend(attributes)
             home_inventory.append(home_detail)
 
@@ -65,5 +89,24 @@ def scrapeData():
 
 
 
+def clean_data(home_csv):       
+    home_data = pd.read_csv(home_csv, encoding='latin1', na_values='n/a')
+    home_data['price'] = home_data[['price']].applymap(atof)
+    home_data = home_data.dropna()
+    home_data['square_feet'] = home_data[['square_feet']].applymap(atof)
+    home_data.to_csv("home_detail-cleaned.csv", index=False)
+    
+    return
+
+def clean_data_with_year(home_csv):
+    home_data = pd.read_csv(home_csv, encoding='latin1', na_values='n/a')
+    home_data['price'] = home_data[['price']].applymap(atof)
+    home_data = home_data.dropna()
+    home_data.to_csv("home_detail-cleaned-year.csv", index=False)
+
+
+
 if __name__=='__main__':
-    scrapeData()
+    # scrapeData()
+    home_data_string = "data-with-year.csv"
+    clean_data_with_year(home_data_string)
